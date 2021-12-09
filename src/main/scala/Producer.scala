3b4edb8975e101086164a4c56c298a2be4d56d54
github-lucas-plaetevoet <lucas.plaetevoet@teads.com>
2021-12-09 18:05:01 +0100

    put connector config in separate file

--- src/main/scala/Producer.scala
+++ src/main/scala/Producer.scala
@@ -17,7 +17,7 @@ object Producer extends App {
 
   val kafkaBootstrapServer = "kafka:29092"
   val schemaRegistryUrl = "http://schema-registry:8081"
-  val topicName = "demossp2"
+  val topicName = "demossp3"
 
   val props = new Properties()
   props.put("bootstrap.servers", kafkaBootstrapServer)
